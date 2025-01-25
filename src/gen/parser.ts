1ea966bdbefe92150b4cbb45f28407c8fff20b87
Nathan Bolton <github@thinknathan.com>
2025-01-25 06:43:38 +0000

    feat: include `liveupdate` API by checking its namespace (#15)
    * chore: modernize editor settings
    
    * feat: include liveupdate API by name only
    
    * fix: typo
    
    * fix: typo
    
    * fix: variable name

--- src/gen/parser.ts
+++ src/gen/parser.ts
@@ -95,6 +95,10 @@ const defaults = [
   { group: schema.EDocGroup.Extensions },
   { group: schema.EDocGroup.Lua, includes: ['socket'] },
 ];
+// Groups are not guaranteed to be set in the Defold API
+// Instead, we include some APIs by checking their name
+const includedApisByNamespace = ['liveupdate'];
+
 export function parse(
   input: Array<schema.IDocJson>,
   groups: Array<schema.IDocGroupFilter> = defaults,
@@ -105,6 +109,12 @@ export function parse(
     .slice()
     .sort((a, b) => a.info.namespace.localeCompare(b.info.namespace));
   const filtered = alphabetical.filter((doc) => {
+    if (
+      doc.info.namespace &&
+      includedApisByNamespace.includes(doc.info.namespace)
+    ) {
+      return true;
+    }
     const group = groups.find((g) => g.group == doc.info.group);
     if (group) {
       if (group.include) {
